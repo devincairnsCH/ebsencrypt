f8db426e82fb16c93b213a06750b5c6e64fb843c
devincairnsCH <devin.cairns@cloudhesive.com>
2022-04-20 15:26:25 -0400

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 wget https://github.com/jbrt/ec2cryptomatic/releases/download/2.3.2/ec2cryptomatic.linux.amd64.zip
 unzip ec2cryptomatic.linux.amd64.zip
-wget encryptEBS.sh
+wget https://raw.githubusercontent.com/devincairnsCH/ebsencrypt/main/encryptEBS.sh
 chmod +x encryptEBS.sh
